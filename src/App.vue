728e48e0e4bad1bcb71028c3ce11af5fdb4752fd
Gla3nata <0207natalya@gmail.com>
2026-01-21 13:21:29 +0600

    adding layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <div id="app">
-    Hello Vue.js
-  </div>
+    <div class="wrapper">
+        
+        <hero-view />
+        <footer-component />
+    </div>
 </template>
 
 <script>
+import HeroView from './views/HeroView.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
-
-export default {  
+export default {
+    components: {
+        HeroView,
+        FooterComponent
+    }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
