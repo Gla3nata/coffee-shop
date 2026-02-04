523f5461fff248064b42535d2213bf60f7db307e
Gla3nata <0207natalya@gmail.com>
2026-02-04 11:43:32 +0600

    lesson 8, make links

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 
 import router from './router'
+import store from './store'
 
 import './assets/scss/style.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
